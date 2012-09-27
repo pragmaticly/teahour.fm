e9f1f1694a3dae5aed426cbd1ea2f52500600b63
fujimogn <fujimogn@gmail.com>
2012-09-28 03:20:11 +0900

    fixed

--- Gemfile
+++ Gemfile
@@ -3,9 +3,10 @@ ruby "1.9.3"
 source :rubygems
 
 gem "thin"
-gem "middleman", "~>3.0.5"
-gem 'middleman-livereload'
 gem "rack-contrib"
+gem "middleman", "~>3.0.5"
+gem "middleman-livereload"
+gem "middleman-smusher"
 
 gem "jquery-sprockets", :git => "git://github.com/ent-io/jquery-sprockets.git", :branch =>"dev"
 gem "bootstrap-sass"
@@ -14,4 +15,8 @@ gem "slim", "~> 1.3.2"
 group :development do
   gem 'heroku'
   gem 'foreman'
+end
+
+group :production do
+  gem 'therubyracer-heroku'
 end
--- config.rb
+++ config.rb
@@ -64,10 +64,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
@@ -77,11 +77,12 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
 
 activate :livereload
+activate :gzip
