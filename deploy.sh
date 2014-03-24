202cd7343617a93498ae0ed5973f8041eca8ac86
Dingding Ye <yedingding@gmail.com>
2014-03-24 20:34:41 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 middleman build
-middleman deploy
-cd build && git add . && git commit -m "update" && git push -f gitcafe gitcafe-pages
+cd build && git add . && git commit -m "update" && git push -f origin gh-pages && git push -f gitcafe gh-pages:gitcafe-pages
 cd ../
